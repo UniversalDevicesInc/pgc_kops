20123c392c67ede5a589b155ce88423cf11c2fae
Einsten42 <milne.james@gmail.com>
2019-05-20 18:23:12 -0500

    update

--- cloud42/kops_install.sh
+++ cloud42/kops_install.sh
@@ -8,6 +8,11 @@
 # IAMFullAccess
 # AmazonVPCFullAccess
 
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install git -y
+
+
 NAME=kube.aws.cloud42.dev
 KOPS_STATE_STORE=s3://aws-cloud42-dev-state-store
 
@@ -16,6 +21,8 @@ echo "Installing kubectl....."
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 # kops install
 echo "Installing kops....."
@@ -25,10 +32,10 @@ sudo mv kops-linux-amd64 /usr/local/bin/kops
 
 # Add environment to .bashrc
 echo "Setting environment variables....."
-echo .bashrc >> export NAME=$NAME
-echo .bashrc >> export KOPS_STATE_STORE=$KOPS_STATE_STORE
 export NAME=$NAME
 export KOPS_STATE_STORE=$KOPS_STATE_STORE
+echo "export NAME=$NAME" >> ~/.bashrc
+echo "export KOPS_STATE_STORE=$KOPS_STATE_STORE" >> ~/.bashrc
 
 # kops configure system
 kops create cluster --master-count 1 --node-count 2 --zones us-east-1a,us-east-1b \
