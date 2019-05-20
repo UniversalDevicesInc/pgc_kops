ab5035e901fcf75ac601dd5901885a1c3d57fd0f
Einsten42 <milne.james@gmail.com>
2019-05-20 17:54:49 -0500

    scripts

---
+++ shared/install_accessories.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Dashboard install
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+
+# helm install
+curl -LO https://git.io/get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install helm
+helm init --service-account tiller --history-max 200
