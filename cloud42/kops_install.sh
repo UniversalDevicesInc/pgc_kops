5902650d09636e2c5c0ef194f89337c1303599d2
Einsten42 <milne.james@gmail.com>
2019-05-21 12:33:50 -0500

    update

--- cloud42/kops_install.sh
+++ cloud42/kops_install.sh
@@ -34,7 +34,7 @@ export KOPS_STATE_STORE=$KOPS_STATE_STORE
 
 
 # kops configure system
-kops create cluster --master-count 1 --node-count 2 --zones us-east-1a,us-east-1b \
+kops create cluster --master-count 1 --node-count 1 --zones us-east-1a # ,us-east-1b \
   --master-zones us-east-1a
 #  -t private --networking weave \
 # --node-size t2.large \
