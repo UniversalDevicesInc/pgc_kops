e9b13503cf3a70c86e81bfb1942eccee6cd3063a
Einsten42 <milne.james@gmail.com>
2019-05-25 13:41:11 -0500

    build update

--- shared/install_accessories.sh
+++ shared/install_accessories.sh
@@ -5,15 +5,15 @@ kubectl apply -f ./namespaces_and_serviceaccount.yaml
 # Dashboard install
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
 
-# helm install
-curl -LO https://git.io/get_helm.sh
-chmod 700 get_helm.sh
-./get_helm.sh
+# get helm
+# curl -LO https://git.io/get_helm.sh
+# chmod 700 get_helm.sh
+# ./get_helm.sh
 
 # install helm
-helm init --service-account tiller --history-max 200
-helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
-helm repo update
+# helm init --service-account tiller --history-max 200
+# helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+# helm repo update
 aws logs create-log-group --log-group-name kubernetes.pgc-system --region us-east-1
 aws logs create-log-group --log-group-name kubernetes.nodeservers --region us-east-1
 aws logs create-log-group --log-group-name kubernetes.system --region us-east-1
@@ -23,5 +23,6 @@ aws logs put-retention-policy --log-group-name kubernetes.nodeservers \
   --retention-in-days 30 --region us-east-1
 aws logs put-retention-policy --log-group-name kubernetes.system \
   --retention-in-days 30 --region us-east-1
-helm install --name fluentd incubator/fluentd-cloudwatch \
-  -f ./fluentd.yaml --namespace kube-system
+#helm install --name fluentd incubator/fluentd-cloudwatch \
+#  -f ./fluentd.yaml --namespace kube-system
+# kubectl apply -f ./
---
+++ shared/kops_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# AWS Linux2 AMI
+# EC2 role
+# AmazonEC2FullAccess
+# AmazonRoute53FullAccess
+# AmazonS3FullAccess
+# IAMFullAccess
+# AmazonVPCFullAccess
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+[ "$#" -eq 1 ] || die "cluster FQDN required e.g. 'pgcdev.aws.cloud42.dev', $# provided"
+[ "$#" -eq 2 ] || die "kops config spec required e.g. 'pgcdev.yaml', $# provided"
+NAME=$1
+DNAME=${NAME//./-}
+KOPS_STATE_STORE=s3://${DNAME}-state-store
+export NAME=$NAME
+export KOPS_STATE_STORE=$KOPS_STATE_STORE
+
+# AWS Create Buckets and Cloudwatch Logs Groups
+aws s3api create-bucket --acl public-read --bucket ${DNAME}-ns-logs --region us-east-1
+aws s3api create-bucket --acl public-read --bucket ${DNAME}-state-store --region us-east-1
+
+aws logs create-log-group --log-group-name ${NAME}.pgc-system --region us-east-1
+aws logs create-log-group --log-group-name ${NAME}.nodeservers --region us-east-1
+aws logs create-log-group --log-group-name ${NAME}.system --region us-east-1
+aws logs put-retention-policy --log-group-name ${NAME}.pgc-system \
+  --retention-in-days 30 --region us-east-1
+aws logs put-retention-policy --log-group-name ${NAME}.nodeservers \
+  --retention-in-days 30 --region us-east-1
+aws logs put-retention-policy --log-group-name ${NAME}.system \
+  --retention-in-days 30 --region us-east-1
+
+# kubectl install
+echo "Installing kubectl....."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# kops install
+echo "Installing kops....."
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+# Add environment to .bashrc
+echo "Setting environment variables....."
+echo "export NAME=$NAME" >> ~/.bashrc
+echo "export KOPS_STATE_STORE=$KOPS_STATE_STORE" >> ~/.bashrc
+source ~/.bashrc
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+source <(kops completion bash)
+echo "source <(kops completion bash)" >> ~/.bashrc
+
+
+# kops configure system ,us-east-1b \
+kops create -f $2
+#  -t private --networking weave \
+# --node-size t2.large \
+# --master-size t2.medium $NAME
+
+# modify cluster config if needed
+# kops edit ig $NAME
+
+# modify master config if necessary
+# kops edit ig --name=$NAME master-us-east-1a
+
+# modify nodes config if needed
+# kops edit ig --name=kube.aws.cloud42.dev nodes
+
+# deploy cluster on AWS
+# kops update cluster $NAME --yes
