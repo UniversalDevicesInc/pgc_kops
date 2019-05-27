48d4634a5a744cf5c48bcde6efcc04bc6524dbe6
Einsten42 <milne.james@gmail.com>
2019-05-27 18:04:51 -0500

    updated cdp

--- shared/userdata.sh
+++ shared/userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum update -y
 yum upgrade -y
-yum install git -y
+yum install git epel-release jq -y
 cd /home/ec2-user
 su ec2-user -c "ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ''"
 su ec2-user -c "git clone https://github.com/UniversalDevicesInc/pgc_kops.git"
